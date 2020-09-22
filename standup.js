bed15aec528275467d53e13da9c2d57170e72c6f
Ronak Shah <ronakshahultimate@gmail.com>
2020-09-21 18:03:42 -0700

    finished mvp

--- standup.js
+++ standup.js
@@ -5,6 +5,7 @@ const bot = new Discord.Client();
 global.bot = bot;
 bot.commands = new Discord.Collection();
 const botCommands = require('./commands');
+const moment = require('moment');
 
 Object.keys(botCommands).map(key => {
   bot.commands.set(botCommands[key].name, botCommands[key]);
@@ -17,5 +18,8 @@ bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
-  bot.channels.get(STANDUP_CHANNEL_ID).send("<@Engineering>, what did you work on?");
+  const day = moment().day();
+  if (day === 3 || day === 6) {
+    bot.channels.get(STANDUP_CHANNEL_ID).send(`<@&750611564574408787>, what did you work on? ${day}`);
+  }
 });
